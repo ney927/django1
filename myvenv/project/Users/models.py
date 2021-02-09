fb989f76721a74f863bbec0badb15a85527c28bb
ney927 <nbilling9@yahoo.com>
2021-02-09 13:27:55 -0500

    change to postgres db

--- myvenv/project/Users/models.py
+++ myvenv/project/Users/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+import django.contrib.postgres.fields
+from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE, )
-  pins = ArrayField(models.FileField(), blank = True)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  pins = ArrayField(models.FileField(), blank = True, default = list)
 
 class File(models.Model):
   file = models.FileField()
-  tags = ArrayField(models.CharField(max_length=200), blank=True)
+  tags = ArrayField(models.CharField(max_length=200), blank=True, default=list)
--- myvenv/project/project/settings.py
+++ myvenv/project/project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'Users',
 ]
 
 MIDDLEWARE = [
@@ -73,10 +75,21 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neyhabilling',
+        'USER': 'neyhabilling',
+        'PASSWORD': 'awesomeN2',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
