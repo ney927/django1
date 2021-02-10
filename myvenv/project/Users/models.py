bbb7ca1970725fbad3bc4c514f25deed80f3c73a
Neyha <78521142+ney927@users.noreply.github.com>
2021-02-09 19:18:59 -0500

    startapp Files

--- myvenv/project/Users/models.py
+++ myvenv/project/Users/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-import django.contrib.postgres.fields
-from django.contrib.postgres.fields import ArrayField
+# import django.contrib.postgres.fields
+# from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
-  pins = ArrayField(models.FileField(), blank = True, default = list)
 
 class File(models.Model):
   file = models.FileField()
-  tags = ArrayField(models.CharField(max_length=200), blank=True, default=list)
